a6af8e2141941d9848a03f7efdae2e104a56745a
Aniket Singh <aniket@Krystal.localhost>
2024-06-05 15:29:14 +0530

    added codechef

--- index.js
+++ index.js
@@ -27,10 +27,11 @@ app.listen(PORT, () => {
 
 // functions
 
+
+const request = require('request-promise');
+const cheerio = require('cheerio');
 const fs = require("fs");
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const jquery = require("jquery");
+const flatted = require('flatted');
 const moment = require('moment-timezone');
 // Get random questions of specified rating
 
@@ -88,54 +89,61 @@ async function loadContests(sock, numberWa, reply) {
   let upcoming_contests = "*Upcoming Contests*\n\n";
 
   //codeforces
+  try{
+    const url = "https://codeforces.com/api/contest.list?gym=false";
+    const responsecf = await fetch(url);
+    const usrJSON = await responsecf.json();
+    const contests = usrJSON?.result;
+    const contestsize = usrJSON?.result.length;
 
-  const url = "https://codeforces.com/api/contest.list?gym=false";
-  const responsecf = await fetch(url);
-  const usrJSON = await responsecf.json();
-  const contests = usrJSON?.result;
-  const contestsize = usrJSON?.result.length;
-
-  upcoming_contests+="*Codeforces*\n\n";
-  let c=0;
-  for (let i = contestsize - 1; i >= 0; i--) {
-    if (usrJSON?.result[i]?.relativeTimeSeconds <= 0 && c<3) {
-      var myDate = new Date(usrJSON?.result[i]?.startTimeSeconds * 1000);
-      const date = myDate.toLocaleString(undefined, {
-        timeZone: "Asia/Kolkata",
-      });
-      upcoming_contests +=
-        "name : " +
-        usrJSON?.result[i]?.name +
-        "\nTime and date : " +
-        date +
-        "\n\n";
-      c+=1
+    upcoming_contests+="*Codeforces*\n\n";
+    let c=0;
+    for (let i = contestsize - 1; i >= 0; i--) {
+      if (usrJSON?.result[i]?.relativeTimeSeconds <= 0 && c<3) {
+        var myDate = new Date(usrJSON?.result[i]?.startTimeSeconds * 1000);
+        const date = myDate.toLocaleString(undefined, {
+          timeZone: "Asia/Kolkata",
+        });
+        upcoming_contests +=
+          "name : " +
+          usrJSON?.result[i]?.name +
+          "\nTime and date : " +
+          date +
+          "\n\n";
+        c+=1
+      }
     }
   }
-
-  const request = require('request-promise');
-  const cheerio = require('cheerio');
+  catch(error)
+  {
+    upcoming_contests+="Codeforces not available\n\n";
+  }
   
   // atcoder
-  c=0;
-  const atcoder = "https://atcoder.jp/";
-  upcoming_contests+="*Atcoder*\n\n";
-  const response = await request(atcoder);
-  let $ = cheerio.load(response);
-  let text = $(':header:contains("Active Contests"), :header:contains("Upcoming Contests") + div').children('table').children('tbody').children('tr');
-  text.each(function (){
-    if(c==3) return false;
-    const row = $(this).children('td');
-    const time = row.eq(0).find('a').text();
-    const name = row.eq(1).find('a').text();
-    const momentJST = moment.tz(time, 'Asia/Tokyo');
-    const momentIST = momentJST.tz('Asia/Kolkata');
-    const dateTimeIST = momentIST.format('DD/MM/YYYY HH:mm:ss');
-    upcoming_contests +=  "name : "+name+"\nTime and date : "+dateTimeIST+"\n\n";
-    c+=1;
-    return true;
-  });
-  
+  try{
+    c=0;
+    const atcoder = "https://atcoder.jp/";
+    upcoming_contests+="*Atcoder*\n\n";
+    const response = await request(atcoder);
+    let $ = cheerio.load(response);
+    let text = $(':header:contains("Active Contests"), :header:contains("Upcoming Contests") + div').children('table').children('tbody').children('tr');
+    text.each(function (){
+      if(c==3) return false;
+      const row = $(this).children('td');
+      const time = row.eq(0).find('a').text();
+      const name = row.eq(1).find('a').text();
+      const momentJST = moment.tz(time, 'Asia/Tokyo');
+      const momentIST = momentJST.tz('Asia/Kolkata');
+      const dateTimeIST = momentIST.format('DD/MM/YYYY HH:mm:ss');
+      upcoming_contests +=  "name : "+name+"\nTime and date : "+dateTimeIST+"\n\n";
+      c+=1;
+      return true;
+    });
+  }
+  catch(error)
+  {
+    upcoming_contests+="Atcoder not available";
+  }
   send(sock, numberWa, reply, upcoming_contests);
 }
 
@@ -146,37 +154,117 @@ async function checkRating(sock, numberWa, reply, UID) {
   try {
     var data = JSON.parse(fs.readFileSync(filePath));
   } catch (error) {
-    const errorText = "I don't know you!!!\nCan you please add your CF handle.";
+    const errorText = "I don't know you!!!\nCan you please add your cp handles.";
     send(sock, numberWa, reply, errorText);
     return;
   }
-  const url = "https://codeforces.com/api/user.rating?handle=" + data.handle;
-  const response = await fetch(url);
-  const usrJSON = await response.json();
-  if (usrJSON.status === "FAILED") {
-    send(
-      sock,
-      numberWa,
-      reply,
-      "Your Handle is invalid , please enter a valid CF id.",
-    );
-    return;
+  let text ="*Ratings below*\n\n";
+  if(data.hasOwnProperty("codeforces"))
+  {
+    text+= "*Codeforces :*\n\n";    
+    const url = "https://codeforces.com/api/user.info?handles=" + data.codeforces.handle;
+    const response = await fetch(url);
+    const usrJSON = await response.json();
+    if (usrJSON.status !== "FAILED") 
+    {
+      data.codeforces.rating = usrJSON?.result[0]?.rating;
+      data.codeforces.rank  =usrJSON?.result[0]?.rank;
+      text += "User : " + data.codeforces.handle + "\nRating : " + data.codeforces.rating + "\nRank : *" + data.codeforces.rank +"*\n\n";   
+    }
+    else
+    {
+      text += "Codeforces Handle invalid !!!\n\n";
+    }
   }
-  data.rating = usrJSON?.result[usrJSON?.result.length - 1]?.newRating;
-
-  const text =
-    "Your handle is " + data.handle + "\nYour rating is " + data.rating + ".";
-
+  // if(data.hasOwnProperty("atcoder"))
+  // {
+  //   text+= "*Atcoder :*\n\n";    
+  //   const url = "https://www.codechef.com/users/" + data.atcoder.handle;
+  //   const response = await request(url);
+  //   let $ = cheerio.load(response);
+  //   let rating = $('table[class="dl-table mt-2]').children('tr').children('td').eq(1).children('span').eq(0).text();
+  //   let rank = $('div[class="col-md-3 col-sm-12"]').children('h3').children('b');
+  //   console.log(rank);
+  //   data.atcoder.rating = 0;
+  //   data.atcoder.rank = rank.text();
+  //   text += "User : " + data.atcoder.handle + "\nRating : " + data.atcoder.rating + "\nRank : " + data.atcoder.rank +"\n\n";
+  // }
+  if(data.hasOwnProperty("codechef"))
+  {
+    text+= "*Codechef :*\n\n";    
+    const url = "https://www.codechef.com/users/" + data.codechef.handle;
+    const response = await request(url);
+    let $ = cheerio.load(response);
+    let rating = $('div[class="rating-number"]').text();
+    let rank = $('div[class="rating-star"]').children('span');
+    let actualrank = "";
+    rank.each(function(){
+      const sp = $(this).text();
+      actualrank+=sp;
+    })
+    data.codechef.rating = rating;
+    data.codechef.rank =actualrank;
+    text += "User : " + data.codechef.handle + "\nRating : " + data.codechef.rating + "\nRank : " + data.codechef.rank +"\n\n";   
+  }
+  // if(data.hasOwnProperty("leetcode"))
+  // {
+  //   text+= "*Leetcode :*\n\n";    
+  //   const url = "https://leetcode.com/u/" + data.leetcode.handle;
+  //   const response = await request(url);
+  //   let $ = cheerio.load(response);
+  //   let rating = $('div[class="text-label-1 dark:text-dark-label-1 flex items-center text-2xl"]').text();
+  //   data.leetcode.rating = usrJSON?.result[usrJSON?.result.length - 1]?.newRating;
+  //   text += "Your handle is " + data.leetcode.handle + "\nYour rating is " + data.leetcode.rating + ".\n\n";
+  // }
   send(sock, numberWa, reply, text);
   savedata(data, UID);
 }
 
 // Add new UID.json to contests folder
 
-async function addhandle(UID, cfhandle) {
-  const usr = new Object();
-  usr.handle = cfhandle;
-  usr.rating = 0;
+async function addhandle(UID, id, type) 
+{
+  const filePath = "contests/" + UID + ".json";
+  var usr;
+  try {
+    usr = JSON.parse(fs.readFileSync(filePath));
+  } catch (error) {
+    console.log(error);
+      usr = new Object();
+  }
+
+  if(type==="cf")
+  {
+    let cf = new Object();
+    cf.handle = id;
+    cf.rating = 0;
+    cf.rank = "";
+    usr.codeforces = cf; 
+  }
+  else if(type ==="lc")
+  {
+    let lc = new Object();
+    lc.handle = id;
+    lc.rating = 0;
+    lc.rank = "";
+    usr.leetcode = lc;
+  }
+  else if(type ==="cc")
+  {
+    let cc = new Object();
+    cc.handle = id;
+    cc.rating = 0;
+    cc.rank = "";
+    usr.codechef = cc;
+  }
+  else if(type ==="ac")
+  {
+    let ac = new Object();
+    ac.handle = id;
+    ac.rating = 0;
+    ac.rank ="";
+    usr.atcoder = ac;
+  }
   savedata(usr, UID);
 }
 
@@ -279,7 +367,11 @@ async function connectToWhatsApp() {
           captureMessage = "sent an image";
         else if(Message.hasOwnProperty("documentMessage"))
           captureMessage = "sent a document";
-        else return;
+        else{
+          captureMessage = "[Unknown operation performed]...";
+          return;          
+        }
+
         console.log(captureMessage);
         const numberWa = messages[0]?.key?.remoteJid;
         const compareMessage = captureMessage.toLocaleLowerCase();
@@ -296,7 +388,7 @@ async function connectToWhatsApp() {
           send(sock, numberWa, reply, Welcometext);
         } else if (compareMessage === "feebo 1") {
           const handleText =
-            "Enter your CF Handle. reply as 'feebo handle handle_name'\n\nfor eg : feebo handle aniket54";
+            "Enter your Handle. reply as 'feebo handle platform_name handle_name'\n\nAllowed platform names are cf(Codeforces),lc(Leetcode),cc(Codechef),ac(Atcoder)\n\nfor eg : feebo handle cf aniket54";
           send(sock, numberWa, reply, handleText);
         } else if (compareMessage === "feebo 2") {
           checkRating(sock, numberWa, reply, UID);
@@ -315,15 +407,32 @@ async function connectToWhatsApp() {
             let newmessage = compareMessage.split(" ");
             if (newmessage[0] === "feebo") {
               if (newmessage[1] === "handle") {
-                addhandle(UID, newmessage[2]);
-                const success = "Handle saved successfully.";
-                send(sock, numberWa, reply, success);
+                if(newmessage[2] === "cf" || newmessage[2] === "cc" || newmessage[2] === "lc" || newmessage[2] === "ac")
+                {
+                  addhandle(UID, newmessage[3],newmessage[2]);
+                  const success = "Handle saved successfully.";
+                  send(sock, numberWa, reply, success);
+                }
               } else if (newmessage[1] === "get") {
                 let rating, count;
                 rating = newmessage[2];
                 count = newmessage[3];
                 console.log(rating, count);
                 getQuestions(sock, numberWa, reply, rating, count);
+              }
+              else if(newmessage[1]=== "spam")
+              {
+                let n = newmessage[2];
+                let text =""
+                for(let i=3;i<newmessage.length;i++)
+                {
+                  text+=newmessage[i] + " ";
+                }
+                for(let i=0;i<n;i++)
+                {
+                  send(sock,numberWa,reply,text);
+                  sleep(200);
+                }
               }
             }
           } catch (error) {
